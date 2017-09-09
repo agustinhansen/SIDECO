ed96cb574812942f35386ad1c6b4af5d40c4ea0e
agustinhansen <agustinhansen99@gmail.com>
2017-09-08 23:44:06 -0300

    Se agregaron las clases correspondientes

--- sideco/models.py
+++ sideco/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Sistema(models.Model):
+    def lista_empresa():
+    def lista_desempleado():
+    def dar_de_alta_desempleado():
+    def dar_de_alta_empresa():
+    def enviar_informacion_desempleado():
+    def listado_desempleado():
+    def almacenamiento_historico():
+    
+class Persona(models.Model): 
+    DNI = models.CharField(max_length=10)
+    tipo_de_trabajo = models.TextField()
+    fecha_de_nacimiento = models.TextField()
+    
+class Empleado(models.Model):
+    empresa = models.ForeignKey('Empresa')
+
+class Desempleado(models.Model):
+  
+class Empresa(models.Model):
+    CUIL = models.CharField(max_length=12)
+    razon_social = models.TextField()
+    rubro = models.TextField()
+    
+    def contratar_desempleado():
+      
+class OfertaLaboral(models.Model):
+    empresa = models.ForeignKey('Empresa')
+    tipo_de_trabajo_solicitado = models.TextField()
