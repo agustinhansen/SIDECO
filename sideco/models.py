235d4822bd06c238c5947908230328bceff33c31
root <root@debian>
2017-09-16 01:28:10 -0300

    Modificación en models

--- sideco/models.py
+++ sideco/models.py
@@ -4,30 +4,38 @@ from django.db import models
 
 class Sistema(models.Model):
     def lista_empresa():
+        pass     
     def lista_desempleado():
+        pass
     def dar_de_alta_desempleado():
+        pass
     def dar_de_alta_empresa():
+        pass
     def enviar_informacion_desempleado():
-    def listado_desempleado():
+        pass
+    def listado_desempleado(): 
+        pass
     def almacenamiento_historico():
+        pass
     
 class Persona(models.Model): 
     DNI = models.CharField(max_length=10)
     tipo_de_trabajo = models.TextField()
     fecha_de_nacimiento = models.TextField()
     
-class Empleado(models.Model):
+class Empleado('Persona'):
     empresa = models.ForeignKey('Empresa')
 
-class Desempleado(models.Model):
-  
+class Desempleado('Persona'):
+
 class Empresa(models.Model):
-    CUIL = models.CharField(max_length=12)
+    cuil = models.CharField(max_length=12)
     razon_social = models.TextField()
-    rubro = models.TextField()
-    
+    rubro = models.TextField() 
+
     def contratar_desempleado():
-      
+       pass
+
 class OfertaLaboral(models.Model):
     empresa = models.ForeignKey('Empresa')
     tipo_de_trabajo_solicitado = models.TextField()
