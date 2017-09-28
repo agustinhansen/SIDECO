58ef4fc172f9d0c8e8f7aa7f4b32c6bcaf556f1c
alumno <hola@gmail.com>
2017-09-28 08:54:04 -0300

    Formularios V1

--- sideco/views.py
+++ sideco/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .forms import PostForm
+
 # Create your views here.
+
+def sideco_home(request):
+    return render(request, 'sideco/sideco_home.html', {})
+
+def user_new(request):
+        form = PostForm()
+        return render(request, 'sideco/registro.html', {'form': form})
+
